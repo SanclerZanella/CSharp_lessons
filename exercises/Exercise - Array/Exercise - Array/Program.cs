029a2c2872571acb1544df4e713725ccdee37a56
sanclerz <sanclerzjj@gmail.com>
2022-10-07 09:01:40 +0100

    Added new courses notes and exercises

---
+++ exercises/Exercise - Array/Exercise - Array/Program.cs
@@ -0,0 +1,84 @@
+﻿/*
+    Create an arry to hold the days of the week.
+        Task 1: Output what day today is;
+        Task 2: Use a while loop to output all of the days except 'Wednesday'.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise___Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declare an array of strings for the days of week.
+            string[] days_of_week = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+            // Print out which day is today
+            Console.WriteLine("\nToday is: " + days_of_week[3] + "\n");
+
+            // Get the days index, starting by the first index
+            int days_index = 0;
+
+            // Iterate through the days_of_week array
+            while (days_index < days_of_week.Length)
+            {
+
+                // If days_of_week[days_index] is 'Wednesday', skip it
+                if (days_of_week[days_index] == "Wednesday")
+                {
+
+                    // Increment days_index var before skip 'Wednesday'
+                    days_index++;
+
+                    continue;
+
+                }
+
+                // Print all the elements in the days_of_week array, less the element 3 (Wednesday)
+                Console.WriteLine(days_of_week[days_index] + "\n");
+
+                // Increment days_index var
+                days_index++;
+            }
+
+            string[] arr1 = {"Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"};
+            string[] arr2 = {"Jan", "Zulu", "Feb", "Mar", "Alpha", "Omega"};
+
+            Console.WriteLine("Original arr1");
+            foreach(string a1 in arr1)
+            {
+                Console.WriteLine(a1);
+            }
+            Console.WriteLine("\nOriginal arr2\n");
+            foreach (string a2 in arr2)
+            {
+                Console.WriteLine(a2);
+            }
+
+            //Array.Sort(arr2);
+            //Array.Reverse(arr2);
+
+            //Array.Resize(ref arr1, 8);
+            //Array[7] = "Spagetti";
+
+            //Array.Clear(arr1, 0, arr1.Length);
+            //Array.Clear(arr1, 1, arr1.Length -1);
+            //Array.Clear(arr1, 0, arr1.Length -1);
+
+
+            Console.WriteLine("\nChange arr2\n");
+            foreach (string a2 in arr2)
+            {
+                Console.WriteLine(a2);
+            }
+
+        }
+    }
+}
---
+++ exercises/Exercise2 - Array/Exercise2 - Array/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+    int[]arr1 = {5, 6, 9};
+    int[]arr2 = {6, 8, 5};
+    int[] arrRes = new int[2];
+
+    Loop through arr1 and arr2, and find their unique values.
+    Place the unique values into arrRes and output its contents.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2___Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int arrays
+            int[] arr1 = { 5, 6, 9 };
+            int[] arr2 = { 6, 8, 5 };
+
+            // Array with answers
+            int[] arrRes = new int[2];
+
+            // Iteration Through the arr1 and arr2
+            // Iterate through the arr1
+            foreach (int arr1El in arr1)
+            {
+                // Contains is a method to check if a specified value is in an array
+                if (!(arr2.Contains(arr1El)))
+                {
+                    // SetValue is a method to add value to an empty array
+                    arrRes.SetValue(value: arr1El, index: 0);
+                }
+            }
+
+            // Iterate through the arr2
+            foreach (int arr2El in arr2)
+            {
+                if (!(arr1.Contains(arr2El)))
+                {
+                    arrRes.SetValue(value: arr2El, index: 1);
+                }
+            }
+
+            // Sort arrRes to put the answers in order
+            Array.Sort(arrRes);
+
+            // Iterate through arrRes and print out its elements
+            Console.WriteLine("\nThe unique values are:");
+            foreach (int arrResEl in arrRes)
+            {
+                Console.WriteLine(arrResEl);
+            }
+
+        }
+    }
+}
